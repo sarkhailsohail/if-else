b3ac82c678e9aafec43a003c480c061f1477bc01
Jack Ripper <assassainezio@gmail.com>
2024-03-07 17:21:06 +0500

    if else

---
+++ app.js
@@ -0,0 +1,12 @@
+var num1 = 2
+if (num1 > 8) {
+    console.log('Greater then 8')
+}
+if (num1 > 6) {
+    console.log('Greater then 6')
+}
+if (num1 > 2) {
+    console.log('Greater then 2')
+} else {
+    console.log('Less then 2');
+}
